295b72d73013804964a1232e62279ba76248abae
Ibrahimi06 <127303475+Ibrahimi06@users.noreply.github.com>
2025-02-07 19:01:30 -1200

    added comment

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function previewImage(event, imageId) {
     };
     reader.readAsDataURL(file);
 }
+
+function addToCart(productName, productPrice) {
+    const selectedSize = document.querySelector(`input[name="${productName}-size"]:checked`);
+    if (selectedSize) {
+        const productDetails = {
+            name: productName,
+            price: productPrice,
+            size: selectedSize.value
+        };
+        // Simulate adding to cart and redirect to cart.html
+        console.log("Added to cart:", productDetails);
+        alert("Added to cart: " + JSON.stringify(productDetails));
+        window.location.href = 'cart.html'; // Redirect to cart page
+    } else {
+        alert("Please select a size for " + productName + ".");
+    }
+}
